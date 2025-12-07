4eaa3ab63eac09313ff478e7e653a3e0757529e6
RawanSameer0 <rawansmeer4@gmail.com>
2025-12-07 19:59:54 +0300

    first upload

---
+++ NeelyFood/Forms/FrmOrders.Designer.cs
@@ -0,0 +1,112 @@
+﻿namespace NeelyFood
+{
+    partial class FrmOrders
+    {
+        private System.ComponentModel.IContainer components = null;
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabDailyOrders;
+        private System.Windows.Forms.TabPage tabReservationOrders;
+        private System.Windows.Forms.DataGridView dgvDailyOrders;
+        private System.Windows.Forms.DataGridView dgvReservationOrders;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private void InitializeComponent()
+        {
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabDailyOrders = new System.Windows.Forms.TabPage();
+            this.dgvDailyOrders = new System.Windows.Forms.DataGridView();
+            this.tabReservationOrders = new System.Windows.Forms.TabPage();
+            this.dgvReservationOrders = new System.Windows.Forms.DataGridView();
+            this.btnLogin = new System.Windows.Forms.Button();
+            this.tabControl1.SuspendLayout();
+            this.tabDailyOrders.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvDailyOrders)).BeginInit();
+            this.tabReservationOrders.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvReservationOrders)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabDailyOrders);
+            this.tabControl1.Controls.Add(this.tabReservationOrders);
+            this.tabControl1.Location = new System.Drawing.Point(12, 12);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(900, 494);
+            this.tabControl1.TabIndex = 0;
+            // 
+            // tabDailyOrders
+            // 
+            this.tabDailyOrders.BackColor = System.Drawing.Color.Black;
+            this.tabDailyOrders.Controls.Add(this.dgvDailyOrders);
+            this.tabDailyOrders.Location = new System.Drawing.Point(4, 25);
+            this.tabDailyOrders.Name = "tabDailyOrders";
+            this.tabDailyOrders.Size = new System.Drawing.Size(892, 465);
+            this.tabDailyOrders.TabIndex = 0;
+            this.tabDailyOrders.Text = "الطلبات اليومية";
+            // 
+            // dgvDailyOrders
+            // 
+            this.dgvDailyOrders.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dgvDailyOrders.Location = new System.Drawing.Point(0, 0);
+            this.dgvDailyOrders.Name = "dgvDailyOrders";
+            this.dgvDailyOrders.Size = new System.Drawing.Size(892, 465);
+            this.dgvDailyOrders.TabIndex = 0;
+            // 
+            // tabReservationOrders
+            // 
+            this.tabReservationOrders.BackColor = System.Drawing.Color.Black;
+            this.tabReservationOrders.Controls.Add(this.dgvReservationOrders);
+            this.tabReservationOrders.Location = new System.Drawing.Point(4, 25);
+            this.tabReservationOrders.Name = "tabReservationOrders";
+            this.tabReservationOrders.Size = new System.Drawing.Size(892, 465);
+            this.tabReservationOrders.TabIndex = 1;
+            this.tabReservationOrders.Text = "الحجوزات المستقبلية";
+            // 
+            // dgvReservationOrders
+            // 
+            this.dgvReservationOrders.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dgvReservationOrders.Location = new System.Drawing.Point(0, 0);
+            this.dgvReservationOrders.Name = "dgvReservationOrders";
+            this.dgvReservationOrders.Size = new System.Drawing.Size(892, 465);
+            this.dgvReservationOrders.TabIndex = 0;
+            // 
+            // btnLogin
+            // 
+            this.btnLogin.BackColor = System.Drawing.Color.Silver;
+            this.btnLogin.Location = new System.Drawing.Point(700, 512);
+            this.btnLogin.Name = "btnLogin";
+            this.btnLogin.Size = new System.Drawing.Size(205, 33);
+            this.btnLogin.TabIndex = 14;
+            this.btnLogin.Text = " تسجيل الدخول بحساب اخر";
+            this.btnLogin.UseVisualStyleBackColor = false;
+            this.btnLogin.Click += new System.EventHandler(this.btnLogin_Click);
+            // 
+            // FrmOrders
+            // 
+            this.ClientSize = new System.Drawing.Size(924, 562);
+            this.Controls.Add(this.btnLogin);
+            this.Controls.Add(this.tabControl1);
+            this.Name = "FrmOrders";
+            this.Text = "إدارة الطلبات";
+            this.Load += new System.EventHandler(this.FrmOrders_Load);
+            this.tabControl1.ResumeLayout(false);
+            this.tabDailyOrders.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvDailyOrders)).EndInit();
+            this.tabReservationOrders.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvReservationOrders)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        private System.Windows.Forms.Button btnLogin;
+    }
+}
---
+++ NeelyFood/Forms/FrmOrders.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Data;
+using System.Linq;
+using System.Windows.Forms;
+using NeelyFood.Models;
+
+namespace NeelyFood
+{
+    public partial class FrmOrders : Form
+    {
+        private AppDbContext db = new AppDbContext();
+
+        public FrmOrders()
+        {
+            InitializeComponent();
+        }
+
+        private void FrmOrders_Load(object sender, EventArgs e)
+        {
+            this.BackColor = System.Drawing.Color.Black;
+            tabControl1.ForeColor = System.Drawing.Color.Gold;
+
+            LoadDailyOrders();
+            LoadReservationOrders();
+        }
+
+        private void LoadDailyOrders()
+        {
+            var dailyOrders = db.Orders
+                .Where(o => o.IsReservation == false)
+                .Select(o => new
+                {
+                    o.Id,
+                    Customer = o.User.Username,
+                    o.OrderDate,
+                    o.TotalAmount,
+                    o.Status
+                })
+                .ToList();
+
+            dgvDailyOrders.DataSource = dailyOrders;
+            dgvDailyOrders.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
+            dgvDailyOrders.BackgroundColor = System.Drawing.Color.Black;
+            dgvDailyOrders.ForeColor = System.Drawing.Color.Gold;
+            dgvDailyOrders.EnableHeadersVisualStyles = false;
+            dgvDailyOrders.ColumnHeadersDefaultCellStyle.BackColor = System.Drawing.Color.DarkGoldenrod;
+            dgvDailyOrders.ColumnHeadersDefaultCellStyle.ForeColor = System.Drawing.Color.White;
+        }
+
+        private void LoadReservationOrders()
+        {
+            var reservationOrders = db.Orders
+                .Where(o => o.IsReservation == true)
+                .Select(o => new
+                {
+                    o.Id,
+                    Customer = o.User.Username,
+                    o.OrderDate,
+                    o.ReservationDate,
+                    o.TotalAmount,
+                    o.Status
+                })
+                .ToList();
+
+            dgvReservationOrders.DataSource = reservationOrders;
+            dgvReservationOrders.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
+            dgvReservationOrders.BackgroundColor = System.Drawing.Color.Black;
+            dgvReservationOrders.ForeColor = System.Drawing.Color.Gold;
+            dgvReservationOrders.EnableHeadersVisualStyles = false;
+            dgvReservationOrders.ColumnHeadersDefaultCellStyle.BackColor = System.Drawing.Color.DarkGoldenrod;
+            dgvReservationOrders.ColumnHeadersDefaultCellStyle.ForeColor = System.Drawing.Color.White;
+        }
+
+        private void btnLogin_Click(object sender, EventArgs e)
+        {
+            FrmLogin frm = new FrmLogin();
+            frm.Show();
+        }
+    }
+}
